c8e248619ba28817834c62caa32c144969ab565e
alcode <companyhca@gmail.com>
2024-04-01 17:53:42 -0400

    modificacion de .env.local y Comunicacion

--- src/Comunnication/RecojosCom.ts
+++ src/Comunnication/RecojosCom.ts
@@ -2,7 +2,8 @@ import axios, { AxiosResponse } from "axios";
 
 export async function InsertCom(url: string, value: any): Promise<AxiosResponse> {
     try {
-        return await axios.post(`http://localhost:5289/api/${url}`, value);
+        const URI_API_RECOJOS = process.env.URI_API_RECOJOS as string;
+        return await axios.post(`${URI_API_RECOJOS}${url}`, value);
         
     } catch (error) {
         // Maneja los errores de la solicitud POST
